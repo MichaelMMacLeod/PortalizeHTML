898ae93b7f1b38ab53706e99f8f9eca17adb5493
Michael MacLeod <michaelmmacleod@gmail.com>
2024-12-26 17:54:18 -0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        portal_element: "./src/PortalElement.ts",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    optimization: {
+        minimize: false,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                    compress: {
+                        passes: 10,
+                    },
+                    mangle: false,
+                },
+            }),
+        ],
+    },
+    output: {
+        library: '[name]',
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
